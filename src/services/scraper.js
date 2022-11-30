7da518bb29cc91f01d74e4d9f1ad3cefff93fbdb
voluntree <voluntree05@gmail.com>
2022-11-30 15:49:31 +0100

    cambios

--- src/services/scraper.js
+++ src/services/scraper.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const opts = new chrome.Options();
@@ -14,3 +15,29 @@ async function helloSelenium() {
 }
 
 helloSelenium();
+=======
+const { By, Key, Builder, WebElement } = require("selenium-webdriver");
+require("chromedriver");
+
+async function test() {
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  await driver.get("https://www.coordenadas-gps.com/");
+
+  const campoDireccion = await driver.findElement(By.id("address"));
+  const campoLatitud = await driver.findElement(By.id("latitude"));
+  const campoLongitud = await driver.findElement(By.id("longitude"));
+  const botonDireccion = await driver.findElement(
+    By.xpath("//*[@id=\"wrap\"]/div[2]/div[3]/div[1]/form[1]/div[2]/div/button")
+  );
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  await campoDireccion.clear();
+  await campoDireccion.sendKeys("Camino de Vera 22 Valencia");
+  botonDireccion.click();
+}
+
+test();
+>>>>>>> Stashed changes
